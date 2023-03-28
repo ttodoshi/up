c2499f0e4fc7017bf1ab3321b086aee9102e31d2
SaZyGH <103922719+SaZyGH@users.noreply.github.com>
2023-03-28 11:39:34 +0300

    1:12:56

--- WpfApp4/ViewModels/Base/ViewModel.cs
+++ WpfApp4/ViewModels/Base/ViewModel.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace WpfApp4.ViewModels.Base
 {
-    class ViewModel
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+        //~ViewModel()
+        //{
+        //    Dispose(false);
+        //}
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+        }
     }
 }
--- WpfApp4/ViewModels/MainWindowViewModel.cs
+++ WpfApp4/ViewModels/MainWindowViewModel.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfApp4.ViewModels.Base;
 
 namespace WpfApp4.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+        private string _Title = "Анализ статистики WpfApp4";
+
+        /// <summary>Заголок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    if (Equals(_Title, value)) return;
+            //    _Title = value;
+            //    OnPropertyChanged();
+            //
+            //    Set(ref _Title, value);
+            //}
+            set => Set(ref _Title, value);
+        }
+        #endregion
     }
 }
